fe4b0ef5fd4b196063effded028744cb59000864
Akira <dzirt1907@rambler.ru>
2022-05-06 02:47:21 +0300

    Working on dialogues

--- src/components/Dialogues/Dialogues.jsx
+++ src/components/Dialogues/Dialogues.jsx
@@ -1,5 +1,19 @@
-const Dialodues = (props) => {
-  return <div>Dialogues</div>;
+import module from "./Dialogues.module.css";
+const Dialodues = () => {
+  return (
+    <div className={module.dialogues}>
+      <div className={module.dialoguesItems}>
+        <div className={module.dialogue}>Roma</div>
+        <div className={module.dialogue}>Asya</div>
+        <div className={module.dialogue}>Sereja</div>
+      </div>
+      <div className={module.messages}>
+        <div className={module.message}>hi</div>
+        <div className={module.message}>Hello</div>
+        <div className={module.message}>How are you?</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialodues;
