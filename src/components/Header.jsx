790804d7b751f9fb0ea090f9084106c20e9273f5
Akira <dzirt1907@rambler.ru>
2022-05-05 18:30:12 +0300

    added

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://www.logolynx.com/images/logolynx/18/18ca1dd594ed1aa52c1a19cec61540a2.png" />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import { getDefaultNormalizer } from "@testing-library/react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://funart.pro/uploads/posts/2021-04/1617391686_8-p-oboi-spokoinii-fon-8.jpg" />
+      </div>
+      <div>ava + discription</div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+    </div>
+  );
+};
+export default Profile;
